c4c6f08f311c8019d0cf552ad9948ab6aa4e1d79
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-02-05 13:42:51 -0500

    feat: Create book elements and append to library container
    Set book classes and content in order to store info from user

--- script.js
+++ script.js
@@ -21,10 +21,54 @@ function addBookToLibrary(bookObj) {
 
 //displays each book on page
 function displayBooks(){
+    const libraryContainer = document.getElementById("library-container");
+    libraryContainer.innerHTML = ""; //clear existing content
+
+    if(myLibrary.length === 0){ //check if library contains any books
+        libraryContainer.innerHTML = "Add a new book to your library!";
+        console.log("test");
+        return;
+     }
     //iterate through the myLibrary array
     myLibrary.forEach(book => {
-        //access each book and display it on the page
+        //create HTML book elements
+        const bookContainer = document.createElement("div");
+        const bookTitle = document.createElement("h2");
+        const bookAuthor = document.createElement("p");
+        const bookPageCount = document.createElement("p");
+        const bookReadButton = document.createElement("button");
+        const bookDeleteButton = document.createElement("button");
 
+        //set class names for book elements
+        bookContainer.classList.add("book");
+        bookTitle.classList.add("title");
+        bookAuthor.classList.add("author");
+        bookPageCount.classList.add("page-count");
+        bookReadButton.classList.add("read-button");
+        bookDeleteButton.classList.add("delete-button");
+
+        //set book elements' content
+        bookTitle.innerHTML = `title:`;
+        bookAuthor.innerHTML = `Author:`;
+        bookPageCount.innerHTML = `Pages:`;
+        bookReadButton.innerHTML = `Read:`;
+        bookDeleteButton.innerHTML = `Delete`;
+
+
+        //add read button function
+        //add delete button function
+
+        //append book elements to book container
+        bookContainer.appendChild(bookTitle);                
+        bookContainer.appendChild(bookAuthor);                
+        bookContainer.appendChild(bookPageCount);                
+        bookContainer.appendChild(bookReadButton);                
+        bookContainer.appendChild(bookDeleteButton);                
+        //append books to library container
+        libraryContainer.appendChild(bookContainer);
     })
+
+
+    
         
 }
