f82a2360436874aaa51a40ea84a589e227a45be6
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-02-20 00:33:42 -0500

    feat: Display each book on screen

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //array that stores all books
-const myLibrary = [];
+const myLibrary = [{title: "Harry Potter", author: "JK Rolling", numPages: 209, read: "no"}];
 
 //creates new book
 class Book {
@@ -12,63 +12,32 @@ class Book {
         }
     }
 }
-
 //stores new book objects into an array
 function addBookToLibrary(bookObj) {
     myLibrary.push(bookObj);
     return;
 }
-
 //displays each book on page
-function displayBooks(){
-    const libraryContainer = document.getElementById("library-container");
-    libraryContainer.innerHTML = ""; //clear existing content
-
-    if(myLibrary.length === 0){ //check if library contains any books
-        libraryContainer.innerHTML = "Add a new book to your library!";
-        console.log("test");
-        return;
-     }
-    //iterate through the myLibrary array
-    myLibrary.forEach(book => {
-        //create HTML book elements
-        const bookContainer = document.createElement("div");
-        const bookTitle = document.createElement("h2");
-        const bookAuthor = document.createElement("p");
-        const bookPageCount = document.createElement("p");
-        const bookReadButton = document.createElement("button");
-        const bookDeleteButton = document.createElement("button");
-
-        //set class names for book elements
-        bookContainer.classList.add("book");
-        bookTitle.classList.add("title");
-        bookAuthor.classList.add("author");
-        bookPageCount.classList.add("page-count");
-        bookReadButton.classList.add("read-button");
-        bookDeleteButton.classList.add("delete-button");
-
-        //set book elements' content
-        bookTitle.innerHTML = `title:`;
-        bookAuthor.innerHTML = `Author:`;
-        bookPageCount.innerHTML = `Pages:`;
-        bookReadButton.innerHTML = `Read:`;
-        bookDeleteButton.innerHTML = `Delete`;
-
-
-        //add read button function
-        //add delete button function
-
-        //append book elements to book container
-        bookContainer.appendChild(bookTitle);                
-        bookContainer.appendChild(bookAuthor);                
-        bookContainer.appendChild(bookPageCount);                
-        bookContainer.appendChild(bookReadButton);                
-        bookContainer.appendChild(bookDeleteButton);                
-        //append books to library container
-        libraryContainer.appendChild(bookContainer);
-    })
-
-
-    
-        
+function displayBooks(myLibrary){
+    const display = document.getElementById("library-container");
+    //iterate through the array and access each book
+    myLibrary.forEach((book) => {
+        // create a new "card" for each book 
+        const bookCard = document.createElement("div");
+        // create card attributes (title, author, etc) and append them to the card
+        const title = document.createElement("p"); 
+        const author = document.createElement("p"); 
+        const numPages = document.createElement("p"); 
+        const read = document.createElement("p"); 
+        // append those card attributes to the card
+        bookCard.append(title, author, numPages, read);
+        //populate the attributes
+        title.innerText = book.title;
+        author.innerText = book.author;
+        numPages.innerText = book.numPages;
+        read.innerText = book.read;
+        //append bookCard to display
+        display.appendChild(bookCard);
+    });
 }
+displayBooks(myLibrary); // testing
