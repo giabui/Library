e6f953a9e6b342a7b3ae53e461a4fc06f6a40d24
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-02-22 21:56:01 -0500

    feat: Allow user to add new books and display to page using modal window

--- script.js
+++ script.js
@@ -1,74 +1,69 @@
 const display = document.getElementById("library-container");//library books stored here
-const newBookForm = document.getElementById("newBookForm"); //new form stored here
+const openModalButton = document.getElementById("openModal");
+const closeModalButton = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
 
-//array that stores all books
-const myLibrary = [{title: "Harry Potter", author: "JK Rolling", numPages: 209, read: "no"}];
+// activates and deactivates modal 
+openModalButton.addEventListener("click", () => modal.classList.add("open"));
+closeModalButton.addEventListener("click", () => modal.classList.remove("open"));
 
-//creates new book
-class Book {
-    constructor(title, author, numPages, read) {
-        this.title = title;
-        this.author = author;
-        this.numPages = numPages;
-        this.read = () => {
-            //button toggles this
-        }
-    }
-}
-//stores new book objects into an array
-function addBookToLibrary(bookObj) {
-    myLibrary.push(bookObj);
-    // displayBooks(myLibrary);
-    return;
-}
-//displays each book on page
-function displayBooks(myLibrary){
-   
-    //iterate through the array and access each book
+const myLibrary = [];
+
+// displays each book on page
+function displayBooks(){
+    display.innerHTML = "";
+    // iterate through the array and access each book
     myLibrary.forEach((book) => {
         // create a new "card" for each book 
         const bookCard = document.createElement("div");
+
         // create card attributes (title, author, etc) and append them to the card
         const title = document.createElement("p"); 
         const author = document.createElement("p"); 
-        const numPages = document.createElement("p"); 
+        const pages = document.createElement("p"); 
         const read = document.createElement("p"); 
+       
+        // populate the attributes
+        title.innerText = "Title: " + book.title;
+        author.innerText = "Author: " + book.author;
+        pages.innerText = "Pages: " + book.pages;
+        read.innerText = "Read Status: " + book.read;
+
         // append those card attributes to the card
-        bookCard.append(title, author, numPages, read);
-        //populate the attributes
-        title.innerText = book.title;
-        author.innerText = book.author;
-        numPages.innerText = book.numPages;
-        read.innerText = book.read;
-        //append bookCard to display
+        bookCard.append(title, author, pages, read);
+        bookCard.style = "border: 2px solid black";
+
+        // append bookCard to display
         display.appendChild(bookCard);
     });
 }
-displayBooks(myLibrary); // testing
+// displayBooks(myLibrary); // testing
 
+function storeBookData (form) {
+    const formData = new FormData(form);
+    const formDataObj = Object.fromEntries(formData);
 
-// allows user to add a new book to the library
-function getNewBook(){
-    //make sure the form cannot clicke "Add Book Button" until first form is submitted
-   
-    const form = document.createElement("form");
-    form.id = "bookForm";
+    // check if book already in library
+    for (const book of myLibrary){
+        if (book.title === formDataObj.title){
+            // resets form on screen
+            document.getElementById("form").reset();
 
-    const label = document.createElement("label");
-    label.htmlFor = "input";
+            alert("This book already exists in your library. Try adding another.")
+            return;
+        }
+    }
+    // specifies read status on screen for user readability
+    if (formDataObj.read === "on"){
+        formDataObj.read = "Read";
+    }else{
+        formDataObj.read = "Not Read";
+    }
+  
+    myLibrary.push(formDataObj);
+    console.log(myLibrary);
+    displayBooks();
 
-    const input = document.createElement("input");
-    input.name = "input";
-    input.id = "input";
-    input.type = "text";
-    input.placeholder = "Harry Potter";
-    input.required = true;
-    const button = document.createElement("button");
-    button.type = "button";
-
-    button.innerText = "Create Book";
-    form.append(label, input, button);
-    newBookForm.appendChild(form);
-
-    //call addBookToLibrary to add book to library array and display on page
+    // resets form on screen
+    document.getElementById("form").reset();
 }
